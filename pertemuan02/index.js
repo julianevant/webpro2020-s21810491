1813fb14de7b26101464f7429d39a878668820d7
JulianEvant <julianevantwaluyan@gmail.com>
2020-09-29 23:31:23 +0800

    1st commit

---
+++ pertemuan02/index.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+let nama = "Evant"
+let umur = "20"
+
+console.log(nama)
+console.log(umur)
+
+function namaFungsi(){
+    let nama="nama"
+    let umur=20
+    return
+}
+console.log(namaFungsi())
+
+function sapa(){
+    return "hello"
+}
+console.log(sapa())
