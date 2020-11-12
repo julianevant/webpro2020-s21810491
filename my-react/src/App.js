8606034fd24a6d6d39eeeff54474580f47222726
JulianEvant <julianevantwaluyan@gmail.com>
2020-11-12 20:56:52 +0800

    3th commit

---
+++ my-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Hello = () =>
+
+function App() {
+  return (
+    <div>
+      <h1>Basic React</h1>
+      <h2>Component React</h2>
+    </div>
+
+  );
+}
+
+export default App;
